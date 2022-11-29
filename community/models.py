0c6b67cc1fbf146b86f5c056d6ffe7b1be39a30c
한정훈 <tail97@hanmail.net>
2022-11-29 11:30:10 +0900

    db입력 및 저장 실습

--- community/models.py
+++ community/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+    name = models.CharField(max_length=50)
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    url =models.URLField()
+    email = models.EmailField()
+    cdate = models.DateField(auto_now_add= True)
+    
--- community/views.py
+++ community/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
+from .forms import Form
 
 # Create your views here.
 def write(request):
-    #비지니스 로직 구현
-    #data = {'키':'값'}
+    form = Form()
+    
+    # request의 post true이면
+    #사용자가 입력한 form 데이터를 변수에 저장하고
+    # orm으로 db에 저장 
+    #아니면
+    if request.method=='POST':
+        form = Form(request.POST)
+        if form.is_valid():
+            form.save()
+
+    else:
+        form = Form()
     # return render(request,html템플릿파일.html,data)
-    hello = "헬로 django"
-    hello2 = "알고보니 쉬운 장고"
-    return render(request,'write.html', {'data':hello, 'data2':hello2} )
+    return render(request,'write.html', {'form':form} )
